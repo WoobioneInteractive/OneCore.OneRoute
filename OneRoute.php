6fb8908e9ee9a8aecf9e249b4fab8f420dd95c2f
woobione <anton@woobione.se>
2016-11-15 12:40:32 +0100

    Added router

---
+++ OneRoute.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * A fast router for OneCore
+ *
+ * @author A.G. Netterwall <netterwall@gmail.com>
+ */
+class OneRoute implements IPlugin {
+
+	public function Dependencies()
+	{
+
+	}
+
+}
